0bd4c650854cd32c5a5dc694ebcaabc465cd9313
Rodrisc <87383983+Rodrisc@users.noreply.github.com>
2021-07-13 22:06:52 -0300

    Update introdução.c

--- introdução.c
+++ introdução.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 int main(){
+    printf(" ______________________\n");
+    printf("|                      |\n");
+    printf("|                      |\n");
+    printf("|       Bem vindo      |\n");
+    printf("|                      |\n");
+    printf("|  1: Guardar Senhas   |\n");
+    printf("|  2: Excluir Senhas   |\n");
+    printf("|  3: Editar Senhas    |\n");
+    printf("|   4: Gerar Senha     |\n");
+    printf("|   5: Ler senhas      |\n");
+    printf("|                      |\n");
+    printf("|                      |\n");
+    printf("|______________________|\n");
+
     
+    
+
+    return 0;
 }
